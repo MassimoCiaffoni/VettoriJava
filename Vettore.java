8c71314a6b643a0d5bf089fa0f29738748c9508a
MassimoCiaffoni <48352424+MassimoCiaffoni@users.noreply.github.com>
2019-03-09 11:04:27 +0100

    File java

---
+++ Vettore.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Vettore {
+    public static void main(String[] args) {
+    	
+    	Scanner input=new Scanner(System.in);
+    	//creo un arry per memorizzare 10 numeri
+    	int numeri[]= new int[10];
+    	System.out.println("Inserisci 10 numeri");
+    	for(int i=0; i<10;i++) {
+    		numeri[i]= input.nextInt();
+    	}
+    	System.out.println("Il risultato e':");
+    	for(int i=0;i<10;i++)
+    		System.out.println(numeri[i]);
+    	
+    }
+}
